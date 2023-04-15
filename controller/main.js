aded2990f026d71a665a16b3eedbad57b4f9cf77
BisalBosu <bishalbosu@gmail.com>
2023-04-15 10:08:57 +0530

    password is now encrypted

--- controller/main.js
+++ controller/main.js
@@ -1,19 +1,23 @@
 const User = require("../model/registerdUsers")
+const bcrypt = require("bcrypt")
 
 exports.postRegUsers = (req, res, next) => {
 	const email = req.body.email
 	const name = req.body.name
 	const password = req.body.password
 
-	User.create({
-		email: email,
-		name: name,
-		password: password,
-	})
-		.then((user) => {
-			res.json(user)
+	bcrypt.hash(password, 10, async (err, hash) => {
+		if (err) console.log(err)
+		await User.create({
+			email: email,
+			name: name,
+			password: hash
 		})
-		.catch((err) => res.status(409).json(err))
+			.then((user) => {
+				res.json(user)
+			})
+			.catch((err) => res.status(409).json(err))
+	})
 }
 
 exports.postLogIn = (req, res, next) => {
@@ -21,27 +25,41 @@ exports.postLogIn = (req, res, next) => {
 	const password = req.body.password
 
 	User.findByPk(email)
-		.then(user=>{	
-			const real_password = user.password;
+		.then((user) => {
+			const hashed_password = user.password
 
-			if(password != real_password){
-				email = "password not matched";
-				obj={
-					email
-				}
-				return res.status(401).json(obj);
-			}		
+			bcrypt.compare(password, hashed_password, async (err, result)=>{
+				if(err){
+					email = "something went wrong"
+					obj = {
+						email,
+					}
+					return res.status(500).json(obj)
+					}
 
-			obj={
-				email
-			}
-			return res.json(obj);
+					if(result){
+						obj = {
+							email,
+						}
+						return res.json(obj)
+					}
+					
+					else{
+						email = "password not matched"
+					obj = {
+						email,
+					}
+					return res.status(401).json(obj)
+					}
+				}			
+			)
+
 		})
 		.catch((err) => {
 			email = "not found"
 			obj = {
-				email
+				email,
 			}
-			return res.status(404).json(obj);
+			return res.status(404).json(obj)
 		})
 }
