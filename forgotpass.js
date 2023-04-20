29f8093e5333bc729ef9045076b08eafdd9d9bfb
BisalBosu <bishalbosu@gmail.com>
2023-04-20 10:40:48 +0530

    task 14 part 1 frgtpass front end

---
+++ forgotpass.js
@@ -0,0 +1,13 @@
+const url = "http://localhost:3006";
+
+async function forgotPassword(){
+    const email = document.getElementById("email").value;
+
+    obj = {
+        email
+    }
+
+    const response = await axios.post(`${url}/password/forgotpassword`, obj);
+
+
+}
